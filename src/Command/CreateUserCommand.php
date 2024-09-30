dc4603d6bb0da9ec9e407476df16be3c0e44d1a2
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-09-30 18:39:36 +0200

    add user create command

--- src/Command/CreateUserCommand.php
+++ src/Command/CreateUserCommand.php
@@ -2,45 +2,54 @@
 
 namespace App\Command;
 
-use Symfony\Component\Console\Attribute\AsCommand;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 #[AsCommand(
     name: 'CreateUserCommand',
-    description: 'Add a short description for your command',
+    description: 'Create new user account...',
 )]
 class CreateUserCommand extends Command
 {
-    public function __construct()
-    {
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher,
+        private UserRepository $users
+    ) {
         parent::__construct();
     }
+
 
     protected function configure(): void
     {
         $this
-            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
-            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+            ->addArgument('email', InputArgument::REQUIRED, 'email')
+            ->addArgument('password', InputArgument::REQUIRED, 'password')
+
         ;
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
-        $arg1 = $input->getArgument('arg1');
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
 
-        if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
-        }
+        $user = new User();
+        $user->setEmail($email);
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword($user, $password)
+        );
+        $this->users->add($user, true);
 
-        if ($input->getOption('option1')) {
-            // ...
-        }
+
 
         $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
 
